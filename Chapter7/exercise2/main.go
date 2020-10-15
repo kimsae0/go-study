f7a81e250e3a9ec4b2e981c54b2ffe6c0a0db80e
kimsae0 <heehouse1@gmail.com>
2020-10-15 13:31:18 +0900

    chapter 7 ing

---
+++ Chapter7/exercise2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var ok bool
+	var count int
+	data := []string{"a", "b", "e", "a", "e"}
+	counts := make(map[string]int)
+	for _, item := range data {
+		counts[item]++
+	}
+	letters := []string{"a", "b", "c", "d", "e"}
+	for _, letter := range letters {
+		count, ok = counts[letter]
+		if !ok {
+			fmt.Printf("%s: not found\n", letter)
+		} else {
+			fmt.Printf("%s: %d\n", letter, count)
+		}
+	}
+}
