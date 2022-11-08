09c083144836e5c77612914928cae456bb67b20d
kimsae0 <heehouse1@gmail.com>
2022-11-08 19:42:08 +0900

    go mod

---
+++ interface/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/kimsae0/go-study/interface/school"
+	"github.com/kimsae0/go-study/interface/work"
+)
+
+type Rider interface {
+	Ride(vehicle string)
+}
+
+// Sender 인터페이스를 입력으로 받는다
+func RideVehicle(name string, rider Rider) {
+	rider.Ride(name)
+}
+
+func main() {
+
+	schoolRider := &school.SchoolRider{}
+	RideVehicle("버스", schoolRider)
+	RideVehicle("지하철", schoolRider)
+
+	workRider := &work.WorkRider{}
+	RideVehicle("버스", workRider)
+	RideVehicle("지하철", workRider)
+}
